a69b145bb730e108d1d83211b8902f36bccb43c4
Escribe tu nombre sin tildes, y letras como ø, ñ <gsalinase91@gmail.com>
2017-08-01 21:29:13 -0400

    modify element html,in  css, js, & img on folder

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
 $(document).ready(function() {
-  $('#boton-tweets').click(function(event) {
-    var text = $('#text-tweet').val();
-    event.preventDefault();
-        $('#board').append(
-            '<div class="box-tweets">' + 
-                '<div class="media">' + 
-                    '<div class="media-left">' +
-                        '<a href="img/foto-perfil-1.jpg">' +  
-                        '<img class="media-object avatar-tweets" width="48px" src="img/foto-perfil-1.jpg" alt="Foto de Gonzalo Salinas">' +
-                        '</a>' + 
-                    '</div>' + 
-                    '<div class="media-body">' + 
-                        '<strong>Gonzalo Salinas</strong>' +
-                        '<span> @ gsalinase </span>' + 
-                        '<small>.</small>' + 
-                        '<span> 1s </span>' + 
-                        '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +
-                        '<br>' + 
-                        '<p>' + text + '</p>' + 
+    $('#boton-tweets').click(function(event) {
+        var text = $('#text-tweet').val();
+        event.preventDefault();
+            $('#board').prepend(
+                '<div class="box-tweets note">' + 
+                    '<div class="media pin">' + 
+                        '<div class="media-left">' +
+                            '<a href="img/foto-perfil-1.jpg">' +  
+                            '<img class="media-object avatar-tweets" width="48px" src="img/foto-perfil-1.jpg" alt="Foto de Gonzalo Salinas">' +
+                            '</a>' + 
+                        '</div>' + 
+                        '<div class="media-body">' + 
+                            '<strong>Gonzalo Salinas</strong>' +
+                            '<span> @ gsalinase </span>' + 
+                            '<small>.</small>' + 
+                            '<span> 1s </span>' + 
+                            '<div class="boton-eliminar">' +
+                            '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+                            '</div>' +
+                            '<br>' + 
+                            '<p>' + text + '</p>' + 
+                        '</div>' +
+                        '<div class="likeable">' + 
+                          '<span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>' +
+                          '<span class= "contador">0</span>' + 
+                        '</div>' + 
                     '</div>' +
-                    '<div class="likeable">' + 
-                      '<span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>' +
-                      '<span> 0 </span>' + 
-                    '</div>' + 
-                '</div>' +
-            '</div>'
-        );
+                '</div>'
+            );
 
         $('#text-tweet').val(); 
 
         $('#text-tweet').focus();
-  });
+    });
 
+    $(document).on('click', '.boton-eliminar',function(event) {
+     event.stopPropagation();
+      $(this).parents('.note').fadeToggle(1000);
 
+    });
 
-  
+    $(document).on('click', '.likeable', function(event) {
+        $(this).css('color', '#E0245E');  
+        var num = 0;
+        var likeable = $(this).find('span.contador')
+        likeable.text(parseInt(likeable.text())+1)
+    });
+
 });
